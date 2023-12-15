7fa26289883915d4f4ff4fd913711e0004a45cad
Dimitris Vlachopoulos <vlachopoulos@sycada.com>
2023-12-15 11:25:14 +0200

    Initial Commit

---
+++ BlazingTrails.Api/Program.cs
@@ -0,0 +1,34 @@
+using BlazingTrails.Shared.Features.ManageTrails;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Validators;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddFluentValidationClientsideAdapters();
+
+//builder.Services.AddValidatorsFromAssembly(Assembly.Load("BlazingTrails.Shared"));
+builder.Services.AddValidatorsFromAssemblyContaining<TrailValidator>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
